0265386b08bec9f69972504e055f5f1b93e3adcd
Arshadali16 <arshadalishahani16@gmail.com>
2022-02-10 10:24:15 +0000

    new Update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,130 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import Board, { moveCard } from "@lourenci/react-kanban";
+import "@lourenci/react-kanban/dist/styles.css";
+// Use your own styles to override the default styles
+// import "./styles.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const board = {
+  columns: [
+    {
+      id: 1,
+      title: "Backlog",
+      cards: [
+        {
+          id: 1,
+          title: "Card title 1",
+          description: "Card content"
+        },
+        {
+          id: 2,
+          title: "Card title 2",
+          description: "Card content"
+        },
+        {
+          id: 3,
+          title: "Card title 3",
+          description: "Card content"
+        }
+      ]
+    },
+    {
+      id: 2,
+      title: "Doing",
+      cards: [
+        {
+          id: 9,
+          title: "Card title 9",
+          description: "Card content"
+        }
+      ]
+    },
+    {
+      id: 3,
+      title: "Q&A",
+      cards: [
+        {
+          id: 10,
+          title: "Card title 10",
+          description: "Card content"
+        },
+        {
+          id: 11,
+          title: "Card title 11",
+          description: "Card content"
+        }
+      ]
+    },
+    {
+      id: 4,
+      title: "Production",
+      cards: [
+        {
+          id: 12,
+          title: "Card title 12",
+          description: "Card content"
+        },
+        {
+          id: 13,
+          title: "Card title 13",
+          description: "Card content"
+        }
+      ]
+    }
+  ]
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function ControlledBoard() {
+  // You need to control the state yourself.
+  const [controlledBoard, setBoard] = useState(board);
+
+  function handleCardMove(_card, source, destination) {
+    const updatedBoard = moveCard(controlledBoard, source, destination);
+    setBoard(updatedBoard);
+  }
+
+  return (
+    <Board onCardDragEnd={handleCardMove} disableColumnDrag>
+      {controlledBoard}
+    </Board>
+  );
+}
+
+function UncontrolledBoard() {
+  return (
+    <Board
+      allowRemoveLane
+      allowRenameColumn
+      allowRemoveCard
+      onLaneRemove={console.log}
+      onCardRemove={console.log}
+      onLaneRename={console.log}
+      initialBoard={board}
+      allowAddCard={{ on: "top" }}
+      onNewCardConfirm={draftCard => ({
+        id: new Date().getTime(),
+        ...draftCard
+      })}
+      onCardNew={console.log}
+    />
+  );
+}
+
+function App() {
+  return (
+    <>
+      <h4>Example of an uncontrolled board</h4>
+      <UncontrolledBoard />
+      {/* <h4>Example of a controlled board</h4>
+      <p>Just the card moving is implemented in this demo.</p>
+      <p>
+        In this kind of board, you can do whatever you want. We just mirror your
+        board state.
+      </p> */}
+      {/* <ControlledBoard /> */}
+    </>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
